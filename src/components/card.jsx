2d3d050871d8f56efe2639784beb6671d1cc571d
Gabriel Saimo Ferreira Espíndola <gabrielsaimo68@gmail.com>
2021-10-27 03:14:51 -0300

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,5 @@
 import React , {useState} from "react";
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+//import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './laylout/group.css'
